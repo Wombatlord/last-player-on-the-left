53503b0bea6d669830b76812207748d9d3f6f0c2
Wombatlord <jordan.stone0@gmail.com>
2022-02-22 22:40:22 +0000

    Marshalling YAML!

--- src/app/main.go
+++ src/app/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"fmt"	
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/wombatlord/lastplayerontheleft/src/lastplayer"
-	"github.com/wombatlord/lastplayerontheleft/src/rss"
+	"github.com/alexflint/go-arg"
+	"github.com/wombatlord/last-player-on-the-left/src/RSS"
+	"github.com/wombatlord/last-player-on-the-left/src/lastplayer"
+	"gopkg.in/yaml.v2"
 )
 
+var args struct {
+	Alias string `arg:"positional" help:"The RSS feed alias"`
+	Subscribe string `arg:"-s, --subscribe" help:"Supply a URL to the feed to create a subscription with the provided alias"`
+}
+
+type Subscription struct {
+	Url string 
+	Alias string
+}
+
 var (
-	url  = os.Args[1]
+	// url  = os.Args[1]
 	feed RSS.RSSFeed
 )
 
 func main() {
-	content, err := RSS.GetContent(url)
+	arg.MustParse(&args)
+	sub, err := yaml.Marshal(
+		Subscription{
+			Url: args.Subscribe, 
+			Alias: args.Alias,
+		},
+	)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	os.WriteFile("config.yaml", sub, 0644)
+
+	content, err := RSS.GetContent(args.Subscribe)
 	
 	if err != nil {
 		log.Fatalf("error: %v", err)
