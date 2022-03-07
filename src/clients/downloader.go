687e83f72572efa97c6aeb7aac7fb088844b062e
Wombatlord <jordan.stone0@gmail.com>
2022-03-07 06:49:35 +0000

    Logs entries per response from DownloaderMulti(). Goroutine currently causing incorrect package log tag.

--- src/clients/downloader.go
+++ src/clients/downloader.go
@@ -9,11 +9,14 @@ import (
 
 const DownloaderLoggerName = "Downloader"
 
+
 type DownloadClient struct {
 	Client *grab.Client
 }
 
 func NewClient() *grab.Client {
+	logger = app.GetLogChan(DownloaderLoggerName)
+
 	// create a client
 	client := grab.NewClient()
 
@@ -45,13 +48,30 @@ func (c *DownloadClient) DownloadEpisode(client grab.Client, req *grab.Request) 
 }
 
 func (c *DownloadClient) DownloadMulti(client grab.Client, requests ...*grab.Request) {
-	for _, request := range requests {
-		fmt.Printf("Downloading %v... \n", request.URL())
-		c.Client.DoBatch(-1, request)
-		logger <- request.HTTPRequest.Host
-		logger <- request.HTTPRequest.Method
-		logger <- c.Client.UserAgent
-		logger <- request.HTTPRequest.UserAgent()
-		logger <- request.Filename
+	logger = app.GetLogChan(DownloaderLoggerName)
+	responses := c.Client.DoBatch(-1, requests...)
+
+	for elem := range responses {
+		logger <- elem.HTTPResponse.Request.Host
+		logger <- elem.Request.HTTPRequest.UserAgent()
+		logger <- elem.Filename
+		logger <- elem.HTTPResponse.Status
 	}
+	
+	// Only one log message currently being received
+	// logger <- responses.Err().Error()
+	// logger <- responses.HTTPResponse.Request.Host
+	// logger <- responses.Request.HTTPRequest.UserAgent()
+	// logger <- responses.Filename
+	// logger <- responses.HTTPResponse.Status
+	
+	// for _, request := range requests {
+	// 	fmt.Printf("Downloading %v... \n", request.URL())
+	// 	c.Client.DoBatch(-1, request)
+	// 	logger <- request.HTTPRequest.Host
+	// 	logger <- request.HTTPRequest.Method
+	// 	logger <- c.Client.UserAgent
+	// 	logger <- request.HTTPRequest.UserAgent()
+	// 	logger <- request.Filename
+	// }
 }
