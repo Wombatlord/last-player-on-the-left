ec7527dfbebd6bc66c0eadc784658a71d48e8928
tjweldon <tomd90@gmail.com>
2022-03-13 04:12:21 +0000

    Pruned protyping code

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ import (
 var args struct {
 	Alias        string `arg:"positional" help:"The RSS feed alias"`
 	Subscription string `arg:"-s, --subscribe" help:"Supply a URL to the feed to create a subscription with the provided alias"`
-	Latest       bool   `arg:"-l, --latest" help:"Play the latest episode associated to the alias"`
-	Episode      int    `arg:"-e, --episode" help:"Play a specific episode. 0 is the latest episode."`
 	Download     []int  `arg:"-d, --download" help:"Download episodes. 0 is the latest episode."`
 }
 
@@ -26,10 +24,6 @@ var (
 	conf   *app.ConfigFile
 	err    error
 )
-
-func playAudio(url string) {
-	lastplayer.StreamAudio("stream", url)
-}
 
 var AppControllers view.Controllers
 
@@ -90,35 +84,6 @@ func main() {
 		feed, err = clients.GetContent(sub.Url)
 	}
 	fatal(err)
-
-	// If no episode arg is provided, set value to -1 to prevent 0 value instantiation.
-	args.Episode = -1
-	// Prints data from the feed for sanity checking.
-	// feed.EpisodeData(*feed)
-
-	// Main Control flow
-	if args.Subscription != "" {
-		// Handles new Subs
-		fatal(conf.Include(args.Alias, args.Subscription))
-	}
-
-	if args.Latest {
-		// Get the url for the latest episode in the feed.
-		latest := feed.Channel[0].Item[0]
-		streamUrl := latest.Enclosure.Url
-
-		// do the latest noises
-		playAudio(streamUrl)
-	}
-
-	if args.Episode > -1 {
-		// Get the url for the requested episode in the feed.
-		episode := feed.Channel[0].Item[args.Episode]
-		streamUrl := episode.Enclosure.Url
-
-		// do the specific noises
-		playAudio(streamUrl)
-	}
 }
 
 func fatal(err error) {
--- src/lastplayer/lastplayer.go
+++ src/lastplayer/lastplayer.go
@@ -2,30 +2,17 @@ package lastplayer
 
 import (
 	"fmt"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/faiface/beep/speaker"
 	"github.com/rivo/tview"
+	"github.com/wombatlord/last-player-on-the-left/src/app"
+	"github.com/wombatlord/last-player-on-the-left/src/clients"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"time"
-	"unicode"
-
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/effects"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
-	"github.com/gdamore/tcell"
-	"github.com/wombatlord/last-player-on-the-left/src/app"
-	"github.com/wombatlord/last-player-on-the-left/src/clients"
 )
-
-// drawTextLine is a convenience function for drawing text
-func drawTextLine(screen tcell.Screen, x, y int, s string, style tcell.Style) {
-	for _, r := range s {
-		screen.SetContent(x, y, r, nil, style)
-		x++
-	}
-}
 
 var (
 	panel = &AudioPanel{}
@@ -62,14 +49,6 @@ func (ap *AudioPanel) PlayPause() {
 	speaker.Lock()
 	ap.ctrl.Paused = !ap.ctrl.Paused
 	speaker.Unlock()
-}
-
-// InitAudioPanel is a constructor function for the AudioPanel struct.
-func InitAudioPanel(format beep.Format, streamer beep.StreamSeeker) *AudioPanel {
-	getLogger() <- "Building audio panel"
-	panel.subscribers = []PlayerStateSubscriber{}
-	panel.SetStreamer(format, streamer)
-	return panel
 }
 
 // FetchAudioPanel will return the already initialised panel pointer.
@@ -164,216 +143,4 @@ func (ap *AudioPanel) GetPlayerState() PlayerState {
 	}
 
 	return state
-}
-
-// Drawing
-func (ap *AudioPanel) draw(screen tcell.Screen) {
-	mainStyle := tcell.StyleDefault.
-		Background(tcell.NewHexColor(0x473437)).
-		Foreground(tcell.NewHexColor(0xD7D8A2))
-	statusStyle := mainStyle.
-		Foreground(tcell.NewHexColor(0xDDC074)).
-		Bold(true)
-
-	screen.Fill(' ', mainStyle)
-
-	drawTextLine(screen, 0, 0, "This is the Last Player...ON THE LEFT!", mainStyle)
-	drawTextLine(screen, 0, 1, "Press [ESC] to quit.", mainStyle)
-	drawTextLine(screen, 0, 2, "Press [SPACE] to pause/resume.", mainStyle)
-	drawTextLine(screen, 0, 3, "Use keys in (?/?) to turn the buttons.", mainStyle)
-
-	speaker.Lock()
-	position := ap.sampleRate.D(ap.streamer.Position())
-	length := ap.sampleRate.D(ap.streamer.Len())
-	volume := ap.volume.Volume
-	speed := ap.resampler.Ratio()
-	speaker.Unlock()
-
-	positionStatus := fmt.Sprintf("%v / %v", position.Round(time.Second), length.Round(time.Second))
-	volumeStatus := fmt.Sprintf("%.1f", volume)
-	speedStatus := fmt.Sprintf("%.3fx", speed)
-
-	drawTextLine(screen, 0, 5, "Position (Q/W):", mainStyle)
-	drawTextLine(screen, 16, 5, positionStatus, statusStyle)
-
-	drawTextLine(screen, 0, 6, "Volume   (A/S):", mainStyle)
-	drawTextLine(screen, 16, 6, volumeStatus, statusStyle)
-
-	drawTextLine(screen, 0, 7, "Speed    (Z/X):", mainStyle)
-	drawTextLine(screen, 16, 7, speedStatus, statusStyle)
-}
-
-// AppEvent handling
-func (ap *AudioPanel) handle(eventInstance tcell.Event) (changed, quit bool) {
-	switch event := eventInstance.(type) {
-	case *tcell.EventKey:
-		getLogger() <- fmt.Sprintf("Handling AppEvent. Type: %T, Name: %s", eventInstance, event.Name())
-		if event.Key() == tcell.KeyESC {
-			return false, true
-		}
-
-		if event.Key() != tcell.KeyRune {
-			return false, false
-		}
-
-		switch unicode.ToLower(event.Rune()) {
-		case ' ':
-			speaker.Lock()
-			ap.ctrl.Paused = !ap.ctrl.Paused
-			speaker.Unlock()
-			return false, false
-
-		case 'q', 'w':
-			speaker.Lock()
-			newPos := ap.streamer.Position()
-			if event.Rune() == 'q' {
-				newPos -= ap.sampleRate.N(time.Second)
-			}
-			if event.Rune() == 'w' {
-				newPos += ap.sampleRate.N(time.Second)
-			}
-			if newPos < 0 {
-				newPos = 0
-			}
-			if newPos >= ap.streamer.Len() {
-				newPos = ap.streamer.Len() - 1
-			}
-
-			if err := ap.streamer.Seek(newPos); err != nil {
-				log.Fatal(err)
-			}
-			speaker.Unlock()
-			return true, false
-
-		case 'a':
-			speaker.Lock()
-			ap.volume.Volume -= 0.1
-			speaker.Unlock()
-			return true, false
-
-		case 's':
-			speaker.Lock()
-			ap.volume.Volume += 0.1
-			speaker.Unlock()
-			return true, false
-
-		case 'z':
-			speaker.Lock()
-			ap.resampler.SetRatio(ap.resampler.Ratio() * 15 / 16)
-			speaker.Unlock()
-			return true, false
-
-		case 'x':
-			speaker.Lock()
-			ap.resampler.SetRatio(ap.resampler.Ratio() * 16 / 15)
-			speaker.Unlock()
-			return true, false
-		}
-	}
-	return false, false
-}
-
-// StreamAudio is a proof of concept audio playback via tweaked Beep example code.
-// mp3.Decode requires an io.ReadCloser
-// This is provided by os.Open() for local playback
-// audioRequest() provides the io.ReadCloser from a HTTP Response body for streaming from a link.
-func StreamAudio(source string, audioSource string) {
-	switch source {
-	case "local":
-		audioLocal, err := os.Open(audioSource)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		streamer, format, err := mp3.Decode(audioLocal)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer func(streamer beep.StreamSeekCloser) {
-			err := streamer.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(streamer)
-
-		_ = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-		done := make(chan bool)
-		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-			done <- true
-		})))
-		<-done
-
-	case "stream":
-		audio, err := AudioRequest(audioSource)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		streamer, format, err := clients.StreamDecode(audio)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer func(streamer *clients.ClientStreamer) {
-			err := streamer.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(streamer)
-
-		err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		screen, err := tcell.NewScreen()
-		if err != nil {
-			panic(err)
-		}
-		err = screen.Init()
-		if err != nil {
-			panic(err)
-		}
-
-		ap := InitAudioPanel(format, streamer)
-
-		defer func() {
-			screen.Fini()
-		}()
-
-		ap.play()
-
-		// partial prototyping to move away from beep.Seq(... func() {done <- true}) in ap.play()
-		events := make(chan tcell.Event)
-
-		go func() {
-			for {
-				events <- screen.PollEvent()
-			}
-		}()
-
-		t := time.NewTicker(time.Second)
-		defer t.Stop()
-
-	loop:
-		for {
-			select {
-			case event := <-events:
-				changed, quit := ap.handle(event)
-				if quit {
-					getLogger() <- "Exiting"
-					break loop
-				}
-				if changed {
-					screen.Clear()
-					ap.draw(screen)
-					screen.Show()
-				}
-			case <-t.C:
-				screen.Clear()
-				ap.draw(screen)
-				screen.Show()
-			}
-		}
-	}
 }
